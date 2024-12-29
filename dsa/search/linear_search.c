5e6b2b11bfeded10ebbe664a5ac7b66e56022c5c
auster-vn <phutc04@gmail.com>
2024-12-30 03:48:48 +0700

    upload #day1

---
+++ dsa/search/linear_search.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int linesearh(int *arr, int size, int val)
+{
+	int i;
+	for (i=0; i < size; i++)
+	{
+		if (arr[i] == val) return 1;
+        }
+        return 0;
+};
+
+int main(){
+	int n, i, v;
+	printf("Enter the size if the array:\n");
+	scanf("%d", &n);
+
+	int *a = (int *)malloc(n * sizeof(int));
+	printf("Enter the contents for an array of size %d:\n", n);
+	for (int i = 0; i < n; i++)
+          scanf("%d", &a[i]);
+
+        printf("Enter the value to be searched:\n");
+	scanf("%d", &v);
+
+	if(linesearh(a,n,v)) printf("Value %d is in the array.\n",v);
+	else printf("Value %d is not in the array.\n",v);
+
+	free(a);
+	return 0;
+}
